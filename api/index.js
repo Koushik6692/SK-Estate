1d3532f9fea4042e9d2b4e7bb1f519bdd55096ba
Koushik6692 <sarabukoushik09@gmail.com>
2024-03-24 22:17:54 +0530

    edited api/index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("Hello World"));
+// app.get("/", (req, res) => res.send("Hello World"));
 
 app.listen(3000, () => console.log("listening at port 3000!!"));
 
